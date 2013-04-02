07a29f8ef6c0baa7faa96ac942c9bdbe7771dcbb
Eren Güven <erenguven0@gmail.com>
2013-04-02 21:03:07 +0300

    add forgotten exception commit, rename tests test

--- nanomongo/errors.py
+++ nanomongo/errors.py
@@ -1,3 +1,6 @@
 class ValidationError(Exception):
     """Raised when a field fails validation"""
     pass
+
+class ExtraFieldError(Exception):
+	"""Raised when a document has an undefined field"""
